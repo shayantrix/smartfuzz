6ae51890b00e903e6a8dcd1bff16cbe909982829
shayan <shayantrixytb@gmail.com>
2025-05-01 13:37:58 +0330

    Added SQL injection time-base attack

--- pkg/controllers/controllers.go
+++ pkg/controllers/controllers.go
@@ -14,13 +14,17 @@ import (
 	"golang.org/x/net/html"
 )
 
+var (
+	ValidStatusCodes = map[int]bool{200: true, 301: true, 303: true, 307: true, 202: true, 201: true}
+)
+
 func Fuzz() {
 	fmt.Println("Fuzzing target ...")
 }
-
+//Task: Add time base sql injection 
 func URLHandler(urlString string) {
-	payload := Payload("/home/shayan/Desktop/projects/smartfuzz/pkg/controllers/payload.txt")
-	// Sending a Get request; Having the response page HTML code
+	payload := Payload("/home/shayan/Desktop/projects/smartfuzz/pkg/controllers/time_payload.txt")
+	 //Sending a Get request; Having the response page HTML code
 	client := &http.Client{
 		Timeout: 60 * time.Second,
 	}
@@ -31,11 +35,16 @@ func URLHandler(urlString string) {
 		return
 	}
 
-	resp, err := client.Get(u.String())
+	//resp, err := client.Get(u.String())
+	req, err := http.NewRequest("GET", u.String(), nil)
 	if err != nil {
 		fmt.Println("Error in handling the URL: ", err)
 		return
 	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:137.0) Gecko/20100101 Firefox/137.0")
+
+	resp, err := client.Do(req)
 
 	defer resp.Body.Close()
 
@@ -72,19 +81,36 @@ func URLHandler(urlString string) {
 					//func PostForm(url string, data url.Values) (resp *Response, err error)
 
 					for _, item := range payload {
-						response, err := http.Get(u.String() + attr.Val + "=" + item)
+						//Starting time to send payloads
+						//will be used for time-based injection
+						start := time.Now()
+
+						//for some url encodings
+						payloadVal := url.QueryEscape(item)
+						
+						fullUrl := fmt.Sprintf("%s?%s=%s", u.String(), attr.Val, payloadVal)
+
+						response, err := http.Get(fullUrl)
 						if err != nil {
 							fmt.Println("Error in sending request: ", err)
 						}
-						if response.StatusCode != 200 {
+						if !ValidStatusCodes[response.StatusCode] {
 							//Send POST request
 							v := url.Values{}
 							v.Set(attr.Key, item)
+
+							startPost := time.Now()
+							
 							PostRequest, err := http.PostForm(u.String(), v)
 							if err != nil {
 								fmt.Println("Error in sending Post form request: ", err)
-							} else {
+							}
+							if  ValidStatusCodes[PostRequest.StatusCode]{
 								fmt.Println("Found !!!!")
+								elapsedPost := time.Since(startPost)
+								if elapsedPost > 5 * time.Second {
+									fmt.Println("Could be vulnerable to sql injection time-base attack!!!")
+								}
 								fmt.Println(item)
 								//body_req, err := io.ReadAll(PostRequest.Body)
 								//if err != nil{
@@ -92,8 +118,15 @@ func URLHandler(urlString string) {
 								//}
 								//fmt.Println(string(body_req))
 								fmt.Println(PostRequest.StatusCode)
+							}else{
+								fmt.Println("No potential SQL Injection may be found!")
 							}
 						} else {
+							elapse := time.Since(start)
+
+							if elapse > 5 * time.Second{
+								fmt.Println("Could be vulnerable to sql injection time-base attack!!!")
+							}
 							fmt.Println("Found !!!!")
 							fmt.Println(item)
 							//body_request, err := io.ReadAll(response.Body)
@@ -116,9 +149,6 @@ func URLHandler(urlString string) {
 
 	f(doc)
 
-	//for _, item := range payload{
-	//	fmt.Println(item)
-	//}
 }
 
 func Payload(path string) []string {
